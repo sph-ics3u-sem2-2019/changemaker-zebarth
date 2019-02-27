6b9305228aaef126d1874ff7b30b7ed3cb5585f1
MrZebarth <kris.zebarth@ocsb.ca>
2019-02-27 09:54:09 -0500

    Starter code

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,18 @@ public class Main {
 		double payment=in.nextDouble();
 		//TODO: Here is where your code goes
 		double change=payment-cost;
+		int num100s;
+		int num50s;
+		int num20s;
+		int num10s;
+		//How many 100 dollar bills?
+		System.out.println("Your change is "+change);
+		num100s=(int)(change/100);
+		//Find out how much money is left
+		change=change%100;
+		//How many 50s?
+		
+		System.out.println(num100s+" x $100");
 	}
 
 }
